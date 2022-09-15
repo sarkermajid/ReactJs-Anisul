ac3754521494f56f50aee94bce3c2769b1e2ab5c
sarkermajid <majidursarkar333@gmail.com>
2022-09-15 10:01:28 +0600

    use Jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const des = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, est eius ea nobis nostrum eveniet in perferendis maiores dicta, ducimus rem temporibus obcaecati error! Est ut nobis quis dolore inventore.";
+const date = new Date();
+const currentDay = date.getDate();
+const currentMonth = date.getMonth();
+const currentYear = date.getFullYear();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <div>
+      <h1>React Tutorial With Anisul Islam</h1>
+      <p>{des}</p>
+      <span>{currentDay + "/" + currentMonth + "/" + currentYear}</span>
+  </div>
+);
