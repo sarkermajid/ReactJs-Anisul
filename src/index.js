b8e5457811c6de3517364fb338a2321771e9fc5f
sarkermajid <majidursarkar333@gmail.com>
2022-09-15 17:29:20 +0600

    css styling in jsx

--- src/index.js
+++ src/index.js
@@ -7,12 +7,21 @@ const currentDay = date.getDate();
 const currentMonth = date.getMonth();
 const currentYear = date.getFullYear();
 
+const headingStyle = {
+  color: "red",
+  textAlign:"center",
+  fontSize:"3rem",
+  fontWeight:"bold",
+  backgroundColor:"#000",
+  padding:"20px"
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <div>
-      <h1>React Tutorial With Anisul Islam</h1>
-      <p>{des}</p>
+      <h1 style={headingStyle}>React Tutorial With Anisul Islam</h1>
+      <p className='desStyle'>{des}</p>
       <span>{currentDay + "/" + currentMonth + "/" + currentYear}</span>
   </div>
 );
